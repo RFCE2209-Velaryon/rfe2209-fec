8e481a5729db3b85ff394e8fa2196c26373cf9ce
Andrew Fuller <fuller2580@gmail.com>
2022-10-14 19:24:36 -0400

    items and related state and getrelated api call

--- src/components/relateANDcompare/relateANDcompare.jsx
+++ src/components/relateANDcompare/relateANDcompare.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import getProduct from '../../lib/getProduct.js';
+import axios from 'axios';
 
+const {useState, useEffect} = React;
+const apiurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/';
 const Related = () => {
+  let [items, setItems] = useState([{name: 'loading...'}]);
+  let [related, setRelated] = useState([]);
+
+  useEffect(()=>{
+    getProduct().then(res => {
+      setItems(res.data);
+    });
+  },[]);
+
+  let getRelated = (id) => {
+    return axios({
+      method: 'get',
+      url: apiurl+'products/'+id+'/related'
+    });
+  }
   return(
-    <h1>Related Items Component</h1>
+    <>
+      <h1>Related Items Component</h1>
+      <div>{items[0].name}</div>
+    </>
   )
 };
 
