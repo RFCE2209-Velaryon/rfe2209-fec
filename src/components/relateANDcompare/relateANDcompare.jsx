98c66ce83af40cabfa1cbd4e04bccb47d9294bcb
Andrew Fuller <fuller2580@gmail.com>
2022-10-17 13:53:27 -0400

    getting related products data from api, saving it in state, and rendering to screen

--- src/components/relateANDcompare/relateANDcompare.jsx
+++ src/components/relateANDcompare/relateANDcompare.jsx
@@ -7,12 +7,54 @@ const apiurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/';
 const Related = () => {
   let [items, setItems] = useState([{name: 'loading...'}]);
   let [related, setRelated] = useState([]);
+  let [displayItems, setDisplayItems] = useState([<div>loading...</div>]);
 
   useEffect(()=>{
     getProduct().then(res => {
       setItems(res.data);
+      let itemHolder = [];
+      getRelated(res.data[0].id).then((relRes) => {
+        setRelated(relRes.data);
+        relRes.data.forEach(rel => {
+          itemHolder.push(getProductData(rel).then((proData)=> {
+            return (<><div>{proData.data.name}</div><div>{proData.data.category}</div><div>{proData.data.default_price}</div></>);
+          }));
+        })
+        Promise.all(itemHolder).then(items=> {
+          setDisplayItems(items);
+          console.log(items);
+        })
+      }).then(()=>{
+
+      });
     });
   },[]);
+
+  useEffect(()=> {
+    //console.log(displayItems.length);
+  })
+
+  let getProductData = (id) => {
+    return axios({
+      method: 'get',
+      url: apiurl+'products/'+id
+    })
+  }
+
+  let getProductStyle = (id) => {
+    return axios({
+      method: 'get',
+      url: apiurl+'products/'+id+'/styles'
+    })
+  }
+
+  let getProductStars = (id) => {
+    return axios({
+      method: 'get',
+      url: apiurl+'reviews/meta',
+      params: {product_id: id}
+    })
+  }
 
   let getRelated = (id) => {
     return axios({
@@ -23,7 +65,9 @@ const Related = () => {
   return(
     <>
       <h1>Related Items Component</h1>
-      <div>{items[0].name}</div>
+      <div>{displayItems.map((item)=>{
+        return (<div>{item}</div>);
+      })}</div>
     </>
   )
 };
