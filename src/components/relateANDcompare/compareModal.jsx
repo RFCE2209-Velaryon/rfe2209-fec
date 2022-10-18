dd3982bc592fa0202f473c44b85b2e23c20e2279
Andrew Fuller <fuller2580@gmail.com>
2022-10-18 10:49:01 -0400

    compareModal framework

---
+++ src/components/relateANDcompare/compareModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import axios from 'axios'
+
+const CompareModal = (props) => {
+
+
+  return (
+    <div>
+      <div>
+        <div>Comparing:</div>
+        <div>props.curProductName</div>
+        <div>props.comProductName</div>
+      </div>
+      <div>
+        <div>
+          props.allFeatures.map
+        </div>
+      </div>
+    </div>
+  );
+}
