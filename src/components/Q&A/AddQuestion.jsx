57d7341dde506b5af53622400fb0d9cc88ba9d43
Diana <dchong0123@gmail.com>
2022-10-20 13:53:25 -0400

    rendering answers to correct questions

--- src/components/Q&A/AddQuestion.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AddQuestion = () => {
-  return(
-    <button>Add a Question +</button>
-  )
-}
-
-export default AddQuestion;
--- src/components/Q&A/qna.jsx
+++ src/components/Q&A/qna.jsx
@@ -1,45 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SearchBar from "./SearchBar.jsx";
-import QuestionList from "./QuestionList.jsx";
-import AddQuestion from "./AddQuestion.jsx";
+import Questions from "./Questions.jsx";
 
+const apiurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/';
 const QuestionsAndAnswers = ({prodID}) => {
   const [questions, setQuestions] = useState([]);
-  var questionsArray = [];
-  var pageCount = 0;
-  const getReq = (pageCount) => {
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions', {
-        params: {
-          product_id: prodID,
-          page: pageCount,
-          count: 2
-        }
-      })
-        .then((response) => {
-          if (response.data.results && response.data.results.length > 0) {
-            for (var object of response.data.results) {
-              questionsArray.push([object.question_body, object.question_helpfulness, object.question_id]);
-            }
-          }
-          setQuestions([...questions, ...questionsArray]);
-        })
-        .catch((err) => {console.log(err)});
-  };
   useEffect(() => {
     if (prodID) {
-      pageCount++;
-      getReq(pageCount);
-      pageCount++;
-      getReq(pageCount);
+      getQs(prodID, 1, 4)
+        .then((response) => {
+          // console.log('original: ', response.data.results);
+          var questionList = [];
+          response.data.results.forEach((item) => {
+            questionList.push([item.question_id, item.question_body, item.question_helpfulness]);
+          });
+          setQuestions([...questions, ...questionList]);
+        })
+        .catch((error) => console.log('error at getQs:', error));
     }
-  }, [prodID]);
+  }, []);
+  const getQs = (prodID, pageCount, qCount) => {
+    return axios.get(`${apiurl}qa/questions`, {
+      params: {
+        product_id: prodID,
+        page: pageCount,
+        count: qCount
+      }
+
+    })
+  }
   return(
     <div>
       <h1>Q&A Component</h1>
-      <SearchBar />
-      <QuestionList questions={questions} getReq={getReq} pageCount={pageCount}/>
-      <AddQuestion />
+      {/* searchbar */}
+      {questions.length > 0 && <Questions questions={questions}/>}
+      {/* add more questions */}
     </div>
   )
 };
