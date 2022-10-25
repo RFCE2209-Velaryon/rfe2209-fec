755f4d287009197826f0bc5978bf4d4293959067
Andrew Fuller <fuller2580@gmail.com>
2022-10-25 10:18:22 -0400

    draggable carousel!

--- src/components/relateANDcompare/compareModal.jsx
+++ src/components/relateANDcompare/compareModal.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import axios from 'axios'
+import './relateStyle.css';
 
 const CompareModal = (props) => {
 
 
   return (
     <div onClick={()=>props.onClick()} style={{backgroundColor: 'rgba(0,0,0,0.5)', backdropFilter: 'blur(4px)', position:'fixed', zIndex: 10, left: 0, top: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}} className='modalWrapper'>
-      <div style={{backgroundColor: 'white', position:'relative', width: 405+'px', border: 1 + 'px solid black'}} className='modalContent'>
+      <div style={{backgroundColor: 'white', position:'relative', width: 405+'px', height: '200px', border: 1 + 'px solid black', overflowY:'scroll'}} className='modalContent'>
+        <div className='relative'>
+          <button className='modalBtn' onClick={(e) => {console.log('testingModal')}}>&#9733;</button>
+        </div>
         <table>
           <tr style ={{alignItems: 'center', justifyContent: 'center'}}>
             <th style={{textAlign: 'center', width:135+'px'}}>{null}</th>
@@ -19,7 +23,7 @@ const CompareModal = (props) => {
             <th>{props.comProduct.name}</th>
           </tr>
           {props.allFeatures.map((feat, i)=> {
-            return (<tr key = {i}><td style={{textAlign: 'center', width:135+'px'}}>{feat[0]}</td><td style={{textAlign: 'center', width:135+'px'}}>{feat[1]}</td><td style={{textAlign: 'center', width:135+'px'}}>{feat[2]}</td></tr>);
+            return (<tr key = {i}><td style={{textAlign: 'center', width:135+'px', height: '40px'}}>{feat[0]}</td><td style={{textAlign: 'center', width:135+'px'}}>{feat[1]}</td><td style={{textAlign: 'center', width:135+'px'}}>{feat[2]}</td></tr>);
           })}
         </table>
       </div>
