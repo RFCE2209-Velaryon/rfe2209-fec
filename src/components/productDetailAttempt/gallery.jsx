57c97c948cb521198b43fa6dc16a70229837e190
Andrew Fuller <fuller2580@gmail.com>
2022-10-27 20:11:59 -0400

    added stlye images, selector, and indicator.

--- src/components/productDetailAttempt/gallery.jsx
+++ src/components/productDetailAttempt/gallery.jsx
@@ -1,39 +1,17 @@
 import React from "react";
-import axios from 'axios';
 import './productStyle.css';
 
-const {useState, useEffect} = React;
-const apiurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/';
 const Gallery = (props) => {
-  let [currentImage, setCurrentImage] = useState(0);
-  let [currentPhotos, setCurrentPhotos] = useState({photos: []});
-  useEffect(()=>{
-    setCurrentImage(0);
-    getProductStyle(props.product.id).then(res=> {
-      console.log(res);
-      setCurrentPhotos({photos: res.data.results[0].photos});
-    })
-  },[props.product]);
 
-  useEffect(()=> {
-    console.log(currentPhotos);
-  },[currentPhotos]);
-
-  let getProductStyle = (id) => {
-    return axios({
-      method: 'get',
-      url: apiurl+'products/'+id+'/styles'
-    })
-  }
   return (
     <>
     <div className='galleryWrapper'>
       <div className='mainImgWrapper'>
-        {currentPhotos.photos.length ? <img className ='mainImg' src={currentPhotos.photos[currentImage].url}></img>:null}
+        {props.currentPhotos.photos.length ? <img className ='mainImg' src={props.currentPhotos.photos[props.currentImage].url}></img>:null}
       </div>
       <div className='thumbnailWrapper'>
-        {currentPhotos.photos.length ? currentPhotos.photos.map((photo,i)=>{
-          return (<img key={i} onClick={(e)=>{setCurrentImage(i)}} className='thumbnailMain' src={photo.thumbnail_url}></img>)
+        {props.currentPhotos.photos.length ? props.currentPhotos.photos.map((photo,i)=>{
+          return (<img key={i} onClick={(e)=>{props.setCurrentImage(i)}} className='thumbnailMain' style={{border: props.currentImage === i ? 'solid .25em #20c0ff':''}}src={photo.thumbnail_url}></img>)
         }) : null}
       </div>
 
--- src/components/productDetailAttempt/productDetail.jsx
+++ src/components/productDetailAttempt/productDetail.jsx
@@ -4,14 +4,41 @@ import axios from 'axios';
 import Gallery from './gallery.jsx'
 import Info from './info.jsx'
 import './productStyle.css'
+import Style from './styleSelect.jsx';
 
+const apiurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/';
+const {useState, useEffect} = React;
 const ProductDetail = (props) => {
+  let [currentImage, setCurrentImage] = useState(0);
+  let [currentStyle, setCurrentStyle] = useState(0);
+  let [currentStyles, setCurrentStyles] = useState({styles: []});
+  let [currentPhotos, setCurrentPhotos] = useState({photos: []});
+  useEffect(()=>{
+    if(props.product && props.product.id) {
+      setCurrentImage(0);
+      setCurrentStyle(0);
+      getProductStyle(props.product.id).then(res=> {
+        console.log(res);
+        setCurrentStyles({styles: res.data.results});
+        setCurrentPhotos({photos: res.data.results[0].photos});
+      })
+
+    }
+  },[props.product]);
+
+  let getProductStyle = (id) => {
+    return axios({
+      method: 'get',
+      url: apiurl+'products/'+id+'/styles'
+    })
+  }
   return (
 
     <div className='productDetailWrapper'>
-      {props.product.id ? <Gallery product ={props.product}/> : null}
+      {props.product.id ? <Gallery product ={props.product} currentImage={currentImage} currentPhotos={currentPhotos} setCurrentImage={setCurrentImage}/> : null}
       <div className='detailWrapper'>
         {props.product.id ? <Info product = {props.product} /> : null}
+        {currentStyles.styles.length ? <Style product = {props.product} currentStyles={currentStyles} setCurrentPhotos={setCurrentPhotos} setCurrentImage={setCurrentImage} currentStyle={currentStyle} setCurrentStyle={setCurrentStyle}/> : null}
       </div>
     </div>
   );
